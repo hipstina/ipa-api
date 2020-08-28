bcd2ee0397cd76323b3f0644dab67df76f711c0f
Christina P <17805011+hipstina@users.noreply.github.com>
2020-08-27 19:28:15 -0500

    added second filtering toggle and filtered/mapped EBC value to a color

--- scripts/main.js
+++ scripts/main.js
@@ -19,7 +19,11 @@
 
 
 let xhr = new XMLHttpRequest();
-document.getElementById("beerMe").addEventListener("click", makeRequest);
+// document.getElementById("beerMe").addEventListener("click", wipeMenu);
+document.getElementById("ipa").addEventListener("click", wipeMenu);
+document.getElementById("bartender").addEventListener("click", wipeMenu);
+
+
 
 xhr.addEventListener("progress", progressBar);
 xhr.addEventListener("error", requestError);
@@ -40,23 +44,42 @@ function requestError(e) {
 	console.log("An error occurred processing your request. Check the code!");
 }
 
-function makeRequest() {
+function wipeMenu(e) {
+    let root = document.getElementById("root");
+    while (root.hasChildNodes()) {
+        root.removeChild(root.firstChild) 
+    }
+    console.log(root, "cleared!");
+    makeRequest(e);
+}
+
+function makeRequest(e) {
+    
+    
+        
+
+    let filterURL = "https://api.punkapi.com/v2/beers?page=1&per_page=80";
+    if (e.target.id == "ipa") {
+        filterURL += "&beer_name=ipa";
+    } else if (e.target.id === "bartender") {
+        filterURL = "https://api.punkapi.com/v2/beers/random";
+    }
+
 	xhr.open(
 		"GET",
-		"https://api.punkapi.com/v2/beers?page=1&per_page=80 "
+		filterURL
 	);
-	// console.log("sending request!");
+	console.log(`${e.target.id }sending request!`);
 	// console.log("sent request!");
 	xhr.addEventListener("load", function () {
 		console.log("response received!");
 		if (xhr.status === 200) {
 			if (xhr.readyState === XMLHttpRequest.DONE) {
                 let data = JSON.parse(this.response);
-                
                 let app = document.getElementById('root');
                     let resultsCount = document.createElement('h4');
                     resultsCount.setAttribute('class','resultsCount');
-                    resultsCount.textContent = `Total beers found: ${data.length}`;
+                    resultsCount.textContent = `Total beers on tap: ${data.length}`;
                     app.appendChild(resultsCount);        
 
                         let beerMenu = document.createElement('div');
@@ -66,16 +89,17 @@ function makeRequest() {
                         
 				data.forEach((beer) => { 
 
-                    
                         const card = document.createElement('div');
                         card.setAttribute('class', 'card');
                         beerMenu.appendChild(card);
                     
-                        const h2 = document.createElement('h2');
+                        const h2 = document.createElement('h1');
+                        h2.setAttribute('class','beer_name');
                         h2.textContent = beer.name;
                         card.appendChild(h2);
                     
-                        const h4 = document.createElement('h4');
+                        const h4 = document.createElement('p');
+                        h4.setAttribute('class','beer_tagline');
 
                         // remove period at the end of taglines
                         if (beer.tagline.endsWith('.') == true) {
@@ -83,23 +107,61 @@ function makeRequest() {
                         } else {
                             beer.tagline = beer.tagline.substring(0,beer.tagline.length);
                         }
-                        h4.textContent = `${beer.tagline}`;
+                        h4.textContent = `"${beer.tagline}"`;
                         card.appendChild(h4);
-                    
-                        const pABV = document.createElement('p');
+                        
+                        // const separator = document.createElement('hr');
+                        // separator.setAttribute('class','line');
+                        // card.appendChild(separator);
+
+                        const numdata = document.createElement('ul');
+                        numdata.setAttribute('class','beer_data');
+                        
+                        const pABV = document.createElement('li');
                         pABV.setAttribute('class', 'abv');
-                        pABV.textContent = `ABV: ${beer.abv}`;
-                        card.appendChild(pABV);
-                        const pIBU = document.createElement('p');
+                        pABV.textContent = `${beer.abv} %`;
+                        numdata.appendChild(pABV);
+                        const pIBU = document.createElement('li');
                         pIBU.setAttribute('class', 'ibu');
-                        pIBU.textContent = `IBU: ${beer.ibu}`;
-                        card.appendChild(pIBU);
-                        const pDesc = document.createElement('details');
-                        pDesc.setAttribute('class', 'desc');
-                        // beer.description = beer.description.substring(0,160);
-                        pDesc.textContent = `Details...${beer.description}`;
-                        card.appendChild(pDesc);
+                        pIBU.textContent = `${beer.ibu} ibu`;
+                        numdata.appendChild(pIBU);
+                        const hue = document.createElement('li');
+                        hue.setAttribute('class','ebc');
+                        hue.textContent = `\u2B24`;
+                        hue.style.color = `${calcBeerHue()}`;
+                        numdata.appendChild(hue);
+                        card.appendChild(numdata);
+                        const pDesc = document.createElement('p');
+                        const pDescDet = document.createElement('p');
+                        pDesc.setAttribute('class', 'desc hidden');
+                        pDescDet.setAttribute('class', 'desc details');
+                        beer.description = beer.description.substring(0,45);
+                        pDesc.textContent = `${beer.description}...`;
+                        beer.description += beer.description.substring(100,300);
+                        
+
+                        // card.appendChild(pDesc);
                     
+                        function calcBeerHue() {
+                            if (beer.ebc < 7) {
+                                hue.style.color = "#f8eba7"; // l.blonde
+                            } else if (beer.ebc < 13 ) {
+                                hue.style.color = "#ffe041"; // blonde
+                            } else if (beer.ebc < 21 ) {
+                                hue.style.color = "#cc8800 "; // gold
+                            } else if (beer.ebc < 31 ) {
+                                hue.style.color = "#b34a00"; // red
+                            } else if (beer.ebc < 46 ) {
+                                hue.style.color = "#80000b"; // copper
+                            } else if (beer.ebc < 76 ) {
+                                hue.style.color = "#7c4e20"; // brown
+                            } else if (beer.ebc < 76 ) {
+                                hue.style.color = "#341500"; // d.brown
+                            } else {
+                                hue.style.color = "#000"; // black
+                            }
+                        }
+
                         
                     
                     
@@ -111,5 +173,8 @@ function makeRequest() {
 	});
     xhr.send();
 }
+
+
+    
 
 
